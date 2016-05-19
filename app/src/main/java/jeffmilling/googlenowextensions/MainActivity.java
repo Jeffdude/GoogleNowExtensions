ad217451563218c111ba01b019d3b8c403993d93
Jeffdude <jeffmilling@gmail.com>
2016-05-19 02:46:21 +0200

    initial commit

---
+++ app/src/main/java/jeffmilling/googlenowextensions/MainActivity.java
@@ -0,0 +1,46 @@
+package jeffmilling.googlenowextensions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar myToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainDebug", "MainActivity created!");
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public void beginVoiceListener(View view) {
+        Log.d("MainDebug", "MainMicClicked!");
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.d("MainDebug","Settings clicked!");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
